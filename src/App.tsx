6dcd77995d1b8f418f807273e57550f2d5b2f04e
Hanz Bacus <bhanzchester@gmail.com>
2026-04-01 08:36:58 +0800

    fix: analytics route

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,9 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
           <Route path="/analytics" element={<AnalyticsPage />} />
+          {/* Catch-all must always be last */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
